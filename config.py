96e20ed9bca7224561cb1b177e16f6ecd6e176a5
Dominika Zembrzuska <misiaze@gmail.com>
2020-03-19 17:34:40 +0100

    add peer to peer communication over tcp

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
-BLOCK_SIZE = 16  # data block size in bytes
+BLOCK_SIZE = 16  # aes data block size in bytes
+
+SOCKET_BUFSIZE = 4096  # maximum amount of data to be received at once by socket
+SOCKET_HEADSIZE = 8  # size of a socket message header (message type, message length) in bytes
+SOCKET_HEADFORMAT = '4sL'  # formatting of a socket message header
