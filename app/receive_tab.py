29cc4797e7818a22f581f5abf8f7cf721e92e4c7
Mateusz Buchajewicz <theherrlupardo@gmail.com>
2020-04-14 21:40:08 +0200

    [RECIVER_TAB] - listening port * added input for listening port * receiver tab is now disabled until port is not specified * fixed: selected file name is correctly displayed

--- app/receive_tab.py
+++ app/receive_tab.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtCore
-from PyQt5.QtCore import QThread, pyqtSignal, QObject
-from PyQt5.QtWidgets import QWidget, QHBoxLayout, QLabel, QLineEdit, QVBoxLayout, QTabWidget
+from PyQt5.QtCore import QThread, pyqtSignal, QObject, QRegExp
+from PyQt5.QtGui import QRegExpValidator
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QLabel, QLineEdit, QVBoxLayout, QTabWidget, QPushButton, QMessageBox
 
 from app.send_tab.sub_tabs.file_subtab import FileSubTab
 from app.send_tab.sub_tabs.text_subtab import TextSubTab
@@ -31,9 +32,39 @@ class ReceiveTab(QWidget):
 
     def __create_layout(self):
         vertical_layout = QVBoxLayout()
+        vertical_layout.addLayout(self.__init_listening_port())
         vertical_layout.addLayout(self.__init_sender_input())
         vertical_layout.addLayout(self.__init_content_tabs())
         self.setLayout(vertical_layout)
+
+    def __init_listening_port(self):
+        layout = QHBoxLayout()
+        layout.addWidget(QLabel('Listening port'))
+        self.listening_port = QLineEdit()
+        self.listening_port.setValidator(QRegExpValidator(QRegExp('^[0-9]{1,5}$'), self))
+        layout.addWidget(self.listening_port)
+        self.listening_button = QPushButton('Start listening')
+        self.listening_button.clicked.connect(self.__on_listening_button_click)
+        layout.addWidget(self.listening_button)
+        layout.addStretch(1)
+        return layout
+
+    def __on_listening_button_click(self):
+        if not self.listening_port.hasAcceptableInput():
+            QMessageBox.warning(self, 'Error', 'Port should be number from 0 to 99999')
+            return
+        self.__change_button_text()
+        self.__change_tab_enable_state()
+        # todo: start listening on port specified in self.listening_port
+
+    def __change_button_text(self):
+        if self.listening_button.text() == 'Start listening':
+            self.listening_button.setText('Stop listening')
+        else:
+            self.listening_button.setText('Start listening')
+
+    def __change_tab_enable_state(self):
+        self.tabs.setEnabled(not self.tabs.isEnabled())
 
     def __init_sender_input(self):
         layout = QHBoxLayout()
@@ -51,6 +82,7 @@ class ReceiveTab(QWidget):
         self.file_sub_tab = FileSubTab(sending=False)
         self.tabs.addTab(self.file_sub_tab, 'File')
         self.tabs.resize(250, 300)
+        self.tabs.setDisabled(True)
         layout.addWidget(self.tabs)
         return layout
 
